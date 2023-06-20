38b0c19c1de463bc6eed9349c4c2cea7a1f5b4f5
yyupyong <yupyong.kim@gmail.com>
2023-06-20 12:23:20 +0900

    アプリの構成を少し変更

--- app/src/main/java/com/example/wecan_asakatsu/MainActivity.kt
+++ app/src/main/java/com/example/wecan_asakatsu/MainActivity.kt
@@ -1,41 +1,69 @@
 package com.example.wecan_asakatsu
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MovableContent
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.wecan_asakatsu.navigation.AsakatsuNavigation
+import com.example.wecan_asakatsu.navigation.AsakatsuScreens
 import com.example.wecan_asakatsu.ui.theme.WeCan_asakatsuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AsakatsuApp {
-                AsakatsuNavigation()
-            }
+            AsakatsuApp()
         }
     }
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun AsakatsuApp(
-        content: @Composable () -> Unit
     ) {
-        WeCan_asakatsuTheme {
-            // A surface container using the 'background' color from the theme
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                content()
+        val navController = rememberNavController()
+        Scaffold(
+            bottomBar = {
+                BottomNavigation {
+                    BottomNavigationItem(
+                        icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                        selected = navController.currentBackStackEntry?.destination?.route == AsakatsuScreens.HomeScreen.name,
+                        onClick = {
+                            navController.navigate(AsakatsuScreens.HomeScreen.name) {
+                                launchSingleTop = true
+                            }
+                        }
+                    )
+                    BottomNavigationItem(
+                        icon = { Icon(Icons.Default.AccountCircle, contentDescription = null) },
+                        selected = navController.currentBackStackEntry?.destination?.route == AsakatsuScreens.MyPageScreen.name,
+                        onClick = {
+                            navController.navigate(AsakatsuScreens.MyPageScreen.name) {
+                                launchSingleTop = true
+                            }
+                        }
+                    )
+                }
             }
+        ) {
+            AsakatsuNavigation(navController)
         }
     }
 }
--- app/src/main/java/com/example/wecan_asakatsu/navigation/AsakatsuNavigation.kt
+++ app/src/main/java/com/example/wecan_asakatsu/navigation/AsakatsuNavigation.kt
@@ -1,56 +1,20 @@
 package com.example.wecan_asakatsu.navigation
 
-import android.annotation.SuppressLint
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.wecan_asakatsu.ui.screen.home.HomeScreen
 import com.example.wecan_asakatsu.ui.screen.mypage.MypageScreen
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AsakatsuNavigation() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = {
-            BottomNavigation {
-                BottomNavigationItem(
-                    icon = { Icon(Icons.Default.Home, contentDescription = null) },
-                    selected = navController.currentBackStackEntry?.destination?.route == AsakatsuScreens.HomeScreen.name,
-                    onClick = {
-                        navController.navigate(AsakatsuScreens.HomeScreen.name) {
-                            launchSingleTop = true
-                        }
-                    }
-                )
-                BottomNavigationItem(
-                    icon = { Icon(Icons.Default.AccountCircle, contentDescription = null) },
-                    selected = navController.currentBackStackEntry?.destination?.route == AsakatsuScreens.MyPageScreen.name,
-                    onClick = {
-                        navController.navigate(AsakatsuScreens.MyPageScreen.name) {
-                            launchSingleTop = true
-                        }
-                    }
-                )
-            }
-        }
+fun AsakatsuNavigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = AsakatsuScreens.MyPageScreen.name
     ) {
-        NavHost(
-            navController = navController,
-            startDestination = AsakatsuScreens.MyPageScreen.name
-        ) {
-            composable(AsakatsuScreens.HomeScreen.name) { HomeScreen() }
-            composable(AsakatsuScreens.MyPageScreen.name) { MypageScreen() }
-        }
+        composable(AsakatsuScreens.HomeScreen.name) { HomeScreen() }
+        composable(AsakatsuScreens.MyPageScreen.name) { MypageScreen() }
     }
 }
