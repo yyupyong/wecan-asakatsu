ae8294ffe86da5c123632f819fd75d6a9386dc2c
yyupyong <yupyong.kim@gmail.com>
2023-06-20 11:30:44 +0900

    create BottomNavigation composable into AsakatsuNavigation composable

--- app/src/main/java/com/example/wecan_asakatsu/navigation/AsakatsuNavigation.kt
+++ app/src/main/java/com/example/wecan_asakatsu/navigation/AsakatsuNavigation.kt
@@ -1,2 +1,54 @@
 package com.example.wecan_asakatsu.navigation
 
+import android.annotation.SuppressLint
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AsakatsuNavigation() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                    selected = navController.currentBackStackEntry?.destination?.route == AsakatsuScreens.HomeScreen.name,
+                    onClick = {
+                        navController.navigate(AsakatsuScreens.HomeScreen.name) {
+                            launchSingleTop = true
+                        }
+                    }
+                )
+                BottomNavigation {
+                    BottomNavigationItem(
+                        icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                        selected = navController.currentBackStackEntry?.destination?.route == AsakatsuScreens.HomeScreen.name,
+                        onClick = {
+                            navController.navigate(AsakatsuScreens.HomeScreen.name) {
+                                launchSingleTop = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = AsakatsuScreens.MyPageScreen.name
+        ) {
+
+        }
+    }
+}
