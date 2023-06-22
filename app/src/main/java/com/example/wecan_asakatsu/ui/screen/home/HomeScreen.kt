20938b350c34f7f47e1f13e4a387ee986754fc50
yyupyong <yupyong.kim@gmail.com>
2023-06-22 18:21:28 +0900

    create HomeScreenTopbar

--- app/src/main/java/com/example/wecan_asakatsu/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/wecan_asakatsu/ui/screen/home/HomeScreen.kt
@@ -1,9 +1,31 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.wecan_asakatsu.ui.screen.home
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    Scaffold(topBar = {}) {
+
+    }
+
+}
 
 @Composable
-fun HomeScreen(){
-    Text(text = "This is HomeScreen")
+fun HomeScreenTopbar() {
+    TopAppBar(
+        title = { Text(text = "WECAN朝活") },
+        colors = TopAppBarDefaults.smallTopAppBarColors()
+    )
 }
