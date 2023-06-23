2b830138493f49a2b8b1f3df0b7260b4f2f6f5cf
yyupyong <yupyong.kim@gmail.com>
2023-06-23 09:34:42 +0900

    create contomColor called asakatsuGrey

--- app/src/main/java/com/example/wecan_asakatsu/ui/theme/Color.kt
+++ app/src/main/java/com/example/wecan_asakatsu/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.example.wecan_asakatsu.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val AsakatsuGrey = Color(0xFFf6f6f6)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
