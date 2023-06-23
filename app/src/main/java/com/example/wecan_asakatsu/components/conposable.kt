c72a77e288c9512d93a85338fdd688ebab019dca
yyupyong <yupyong.kim@gmail.com>
2023-06-23 10:18:24 +0900

    create Gradationtopappbar

---
+++ app/src/main/java/com/example/wecan_asakatsu/components/conposable.kt
@@ -0,0 +1,18 @@
+package com.example.wecan_asakatsu.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun GradationTopbar(
+){
+  TopAppBar(
+     modifier = Modifier.fillMaxWidth(),
+  ) {
+
+  }
+}
