92fb4487bc1a00b508d9a7c0672b998deb3645ec
yyupyong <yupyong.kim@gmail.com>
2023-06-24 17:35:41 +0900

    change gradationTopappBar

--- app/src/main/java/com/example/wecan_asakatsu/components/conposable.kt
+++ app/src/main/java/com/example/wecan_asakatsu/components/conposable.kt
@@ -1,28 +1,59 @@
 package com.example.wecan_asakatsu.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.contentColorFor
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
+fun GradationTopbarPreview(
+) {
+    GradationTopbar(title = "WECAN朝活")
+}
+
+@Composable
 fun GradationTopbar(
-){
+    title: String,
+    modifier: Modifier = Modifier,
+) {
     val gradient = Brush.verticalGradient(
         colors = listOf(Color.Red, Color.Blue)
     )
-    TopAppBar(
-        modifier = Modifier.background(gradient),
-        backgroundColor = Color.Transparent,
-        contentColor = Color.White,
+
+    Surface(
+        modifier = modifier
+            .height(50.dp)
+            .fillMaxWidth()
+            .background(gradient)
     ) {
-        Text(text = "WECAN朝活")
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            androidx.compose.material3.Text(
+
+                text = title,
+                style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
+            )
+        }
+
     }
- }
+}
 
