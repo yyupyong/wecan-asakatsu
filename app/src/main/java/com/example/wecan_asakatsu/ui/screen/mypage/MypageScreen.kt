2d117a6a6c63b8b7b2be68b06d4dc98ef42cae51
yyupyong <yupyong.kim@gmail.com>
2023-06-27 08:54:48 +0900

    Gradation Topbar into mypage Screen

--- app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
+++ app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
@@ -1,9 +1,19 @@
 package com.example.wecan_asakatsu.ui.screen.mypage
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.example.wecan_asakatsu.components.GradationTopbar
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MypageScreen(){
-    Text(text = "this is Mypage")
+    
+    androidx.compose.material.Scaffold (
+        topBar = { GradationTopbar(title = "WECAN朝活")}
+            ){
+        
+    }
+    
 }
