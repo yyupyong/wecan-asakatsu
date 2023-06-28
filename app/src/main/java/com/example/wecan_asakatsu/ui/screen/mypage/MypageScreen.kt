ff1325b891de0490d67dcfbaeffdacc1dbf1c5df
yyupyong <yupyong.kim@gmail.com>
2023-06-28 19:10:32 +0900

    add actionButton into Gradationtopbar

--- app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
+++ app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
@@ -1,10 +1,16 @@
 package com.example.wecan_asakatsu.ui.screen.mypage
 
 import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.example.wecan_asakatsu.components.GradationTopbar
+import kotlinx.coroutines.coroutineScope
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -13,9 +19,14 @@ fun MypageScreen() {
     androidx.compose.material.Scaffold(
         topBar = {
             GradationTopbar(title = "WECAN") {
-                //ここでドロワーのComposableを渡す
+              IconButton(onClick = {
+                  //ここでドロワー表示のトグルを切リかえ
+              }) {
+                  Icon(Icons.Filled.Menu, contentDescription = "")
+              }
             }
         }
+
 
     ) {
 
