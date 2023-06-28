5b1fa3a810ebc486f4a8203815cf7c964e72defb
yyupyong <yupyong.kim@gmail.com>
2023-06-28 19:12:51 +0900

    add drawer State

--- app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
+++ app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
@@ -4,17 +4,24 @@ import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import com.example.wecan_asakatsu.components.GradationTopbar
 import kotlinx.coroutines.coroutineScope
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MypageScreen() {
+
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+
 
     androidx.compose.material.Scaffold(
         topBar = {
