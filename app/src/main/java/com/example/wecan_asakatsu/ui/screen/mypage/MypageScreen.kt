0531f9468e8411fc1729a3d09c08ba662f8151e2
yyupyong <yupyong.kim@gmail.com>
2023-06-28 19:14:04 +0900

    add coroutineScope

--- app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
+++ app/src/main/java/com/example/wecan_asakatsu/ui/screen/mypage/MypageScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import com.example.wecan_asakatsu.components.GradationTopbar
 import kotlinx.coroutines.coroutineScope
 
@@ -21,6 +22,7 @@ import kotlinx.coroutines.coroutineScope
 fun MypageScreen() {
 
     val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
 
 
     androidx.compose.material.Scaffold(
